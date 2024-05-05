9284fefe0df016ee15f255a247ebaafb9ac26be4
Steven Joseph <steven@damagebdd.com>
2024-05-05 21:53:01 +1000

    Update product manual and whitepaper

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,16 +7,18 @@
 
 	gtag('config', 'G-5QG625RHB7');
 
-	window.onload = init;
-	const toasts = new Toasts();
+	const toasts = new Toasts({
+		offsetX: 20, // 20px
+		offsetY: 20, // 20px
+		gap: 20, // The gap size in pixels between toasts
+		width: 300, // 300px
+		timing: 'ease', // See list of available CSS transition timings
+		duration: '.5s', // Transition duration
+		dimOld: true, // Dim old notifications while the newest notification stays highlighted
+		position: 'top-center' // top-left | top-center | top-right | bottom-left | bottom-center | bottom-right
+	});
 
-	function init(){
-		// the code to be called when the dom has loaded
-		// #document has its nodes
-		hljs.highlightAll();
-
-
-
+	document.addEventListener("DOMContentLoaded", function() {
 		var kycForm = document.getElementById('kycForm');
 		if (kycForm){
 			kycForm.addEventListener('submit', function(event) {
@@ -51,6 +53,17 @@
 					});
 			});
 		}
+		var menuItems = document.querySelectorAll("#mainmenu li");
+
+		menuItems.forEach(function(item) {
+			var link = item.querySelector("a");
+			console.log("loc " , link.getAttribute("href")); 
+			console.log("path " , window.location.pathname); 
+			if (link.getAttribute("href") === window.location.pathname) {
+				item.classList.add("active"); // Add the "active" class if the href matches the current URL path
+			}
+		});
+		hljs.highlightAll();
 		VANTA.GLOBE({
 			el: "#preamble",
 			mouseControls: true,
@@ -65,19 +78,6 @@
 			color2: 0x2d6e45,
 			backgroundColor: 0xffffff
 		})
-
-	}
-	document.addEventListener("DOMContentLoaded", function() {
-		var menuItems = document.querySelectorAll("#mainmenu li");
-
-		menuItems.forEach(function(item) {
-			var link = item.querySelector("a");
-			console.log("loc " , link.getAttribute("href")); 
-			console.log("path " , window.location.pathname); 
-			if (link.getAttribute("href") === window.location.pathname) {
-				item.classList.add("active"); // Add the "active" class if the href matches the current URL path
-			}
-		});
 	});
 
 })(window, document, undefined);
